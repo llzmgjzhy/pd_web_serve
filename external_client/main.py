91166b869275e765499ef153f70eae5fa5938301
AI306 <121@qq.com>
2023-12-10 20:18:29 +0800

    修改

--- external_client/main.py
+++ external_client/main.py
@@ -2,25 +2,29 @@ import asyncio
 from modules import WsClient
 from utils import initial_files_data, start_monitoring
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="客户端发送数据")
 parser.add_argument(
-    dir_path, type=str, default="E:\\Project program\\test", help="需要监视的文件夹路径"
+    "--dir_path", type=str, default="E:\\Project program\\test", help="需要监视的文件夹路径"
 )
 URL = "ws://127.0.0.1:8000/chat/"
-# DIRECTORY_PATH = "E:\\Project program\\test"
+
+args = parser.parse_args()
 
 
 async def main():
+    if not os.path.exists(args.dir_path):
+        print(f"给定的路径不存在: {args.dir_path}")
+        return
     web_client = WsClient(URL)  # 创建 WsClient 类的实例，用于管理 WebSocket 连接
 
     await web_client.one_handle()
-    await initial_files_data(argparse.dir_path, web_client)
+    await initial_files_data(args.dir_path, web_client)
     # 定义其他异步任务
     await asyncio.gather(
         web_client.handler(), start_monitoring(argparse.dir_path, web_client)
     )
-    # asyncio.gather(*tasks)：将多个异步任务聚合在一起，以便它们可以并发运行。
 
 
 if __name__ == "__main__":
--- pddetectionapp/mysql-python.py
+++ pddetectionapp/mysql-python.py
@@ -1,6 +1,6 @@
-'''
+"""
 MySQL建表操作
-'''
+"""
 import os
 import pymysql
 from pymysql import Error
@@ -12,10 +12,9 @@ import json
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
-        connection = pymysql.connect(host=host_name,
-                                     user=user_name,
-                                     password=user_password,
-                                     database=db_name)
+        connection = pymysql.connect(
+            host=host_name, user=user_name, password=user_password, database=db_name
+        )
         print("Connection to MySQL DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
@@ -24,11 +23,11 @@ def create_connection(host_name, user_name, user_password, db_name):
 
 
 # 创建数据表
-def create_table(connection, create_table_sql):
+def create_table(connection, create_table_sql, table_name):
     cursor = connection.cursor()  # 创建游标对象
     try:
         cursor.execute(create_table_sql)
-        print("Table created successfully")
+        print("Table {} created successfully".format(table_name))
     except Error as e:
         print(f"The error '{e}' occurred")
 
@@ -73,72 +72,96 @@ def alter_table_add_timestamp_column(connection):
         cursor.execute("SHOW COLUMNS FROM sample_info LIKE 'data_time'")
         result = cursor.fetchone()
         if not result:
-            cursor.execute("ALTER TABLE sample_info ADD COLUMN data_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+            cursor.execute(
+                "ALTER TABLE sample_info ADD COLUMN data_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+            )
 
         # 检查 sample_data 表中是否存在 data_time 列
         cursor.execute("SHOW COLUMNS FROM sample_data LIKE 'data_time'")
         result = cursor.fetchone()
         if not result:
-            cursor.execute("ALTER TABLE sample_data ADD COLUMN data_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+            cursor.execute(
+                "ALTER TABLE sample_data ADD COLUMN data_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+            )
 
         connection.commit()
 
-def insert_sample_info(connection,  sensor_type, device_type, sampling_rate, sampling_length,
-                       discharge_type):
 
+def insert_sample_info(
+    connection, sensor_type, device_type, sampling_rate, sampling_length, discharge_type
+):
     with connection.cursor() as cursor:
         # SQL执行语句
         sql = """
             INSERT INTO sample_info (sensor_type, device_type, sampling_rate, sampling_length, discharge_type)
             VALUES (%s, %s, %s, %s, %s)
         """
-        cursor.execute(sql, (sensor_type, device_type, sampling_rate, sampling_length, discharge_type))
+        cursor.execute(
+            sql,
+            (sensor_type, device_type, sampling_rate, sampling_length, discharge_type),
+        )
         connection.commit()
         print("Data insert successfully")
         return cursor.lastrowid
 
-def insert_sample_data(connection, sample_info_id, max_peak, phase, other1, other2, waveform):
+
+def insert_sample_data(
+    connection, sample_info_id, max_peak, phase, other1, other2, waveform
+):
     with connection.cursor() as cursor:
         sql = """
             INSERT INTO sample_data (sample_info_id, max_peak, phase, freq, tim, waveform)
             VALUES (%s, %s, %s, %s, %s, %s)
         """
-        cursor.execute(sql, (sample_info_id, max_peak, phase, other1, other2, json.dumps(waveform)))
+        cursor.execute(
+            sql, (sample_info_id, max_peak, phase, other1, other2, json.dumps(waveform))
+        )
         connection.commit()
 
+
 def read_and_store_data(file_path, connection):
-    with open(file_path, 'rb') as file:
+    with open(file_path, "rb") as file:
         # 以下是数据读取部分
         data = file.read(4)
-        sensor_type = struct.unpack('i', data)[0]
+        sensor_type = struct.unpack("i", data)[0]
         data = file.read(4)
-        device_type = struct.unpack('i', data)[0]
+        device_type = struct.unpack("i", data)[0]
         data = file.read(4)
-        sampling_rate = struct.unpack('f', data)[0]
+        sampling_rate = struct.unpack("f", data)[0]
         data = file.read(4)
-        sampling_length = struct.unpack('i', data)[0]
+        sampling_length = struct.unpack("i", data)[0]
         data = file.read(4)
-        discharge_type = struct.unpack('i', data)[0]
+        discharge_type = struct.unpack("i", data)[0]
 
         # # 连接到数据库并插入数据
-        sample_info_id = insert_sample_info(connection, sensor_type, device_type, sampling_rate, sampling_length,
-                           discharge_type)
+        sample_info_id = insert_sample_info(
+            connection,
+            sensor_type,
+            device_type,
+            sampling_rate,
+            sampling_length,
+            discharge_type,
+        )
 
         # 向后移动48个字节
         file.seek(44, 1)
 
         for i in range(1000):
             # 最大幅值
-            max_peak = struct.unpack('f', file.read(4))[0]
+            max_peak = struct.unpack("f", file.read(4))[0]
             # 相位
-            phase = struct.unpack('f', file.read(4))[0]
+            phase = struct.unpack("f", file.read(4))[0]
             # 其他1
-            freq = struct.unpack('f', file.read(4))[0]
+            freq = struct.unpack("f", file.read(4))[0]
             # 其他2
-            tim = struct.unpack('f', file.read(4))[0]
+            tim = struct.unpack("f", file.read(4))[0]
 
-            waveform = [struct.unpack('f', file.read(4))[0] for _ in range(sampling_length)]
-            insert_sample_data(connection, sample_info_id, max_peak, phase, freq, tim, waveform)
+            waveform = [
+                struct.unpack("f", file.read(4))[0] for _ in range(sampling_length)
+            ]
+            insert_sample_data(
+                connection, sample_info_id, max_peak, phase, freq, tim, waveform
+            )
 
 
 def main():
@@ -152,9 +175,9 @@ def main():
     connection = create_connection(host_name, user_name, user_password, db_name)
 
     # Create tables
-    create_table(connection, create_sampinfo_table)
-    create_table(connection, create_sampdata_table)
-    create_table(connection, create_pdresults_table)
+    create_table(connection, create_sampinfo_table, "sample_info")
+    create_table(connection, create_sampdata_table, "sample_data")
+    create_table(connection, create_pdresults_table, "pd_result")
     alter_table_add_timestamp_column(connection)  # 保存数据时间
 
     # dir_path = 'E:\Data\data0824\高频局放数据20230824_001\电缆\巡检异常数据'  # 实验室GIS模拟放电装置数据20231024
@@ -165,11 +188,11 @@ def main():
     # for i in range(20):
     #     read_and_store_data(files[i], connection)
     #     print('完成第{}组数据读取'.format(i))
-    
+
     # Close the connection
     if connection:
         connection.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
